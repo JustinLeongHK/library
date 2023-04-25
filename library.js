2fc39b8cad59febf0616d62b8304746b2f483ec7
Justin Leong <124329491+JustinLeongHK@users.noreply.github.com>
2023-04-25 11:48:23 +0800

    change to class

--- library.js
+++ library.js
@@ -15,11 +15,20 @@ closeModalBtn.addEventListener("click", function () {
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// function Book(title, author, pages, read) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function validateForm() {
